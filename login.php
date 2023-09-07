ef18efce9517dcf75d06827bdee91c853762c11a
albeito <pritampatel731@gmail.com>
2023-09-07 23:44:38 +0530

    login php file created

--- login.php
+++ login.php
@@ -1,3 +1,33 @@
 <?php
-    echo "This is a test"
+// Check if the request is a POST request
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Establish a database connection (update the credentials)
+    $servername = "your_servername";
+    $username = "your_username";
+    $password = "your_password";
+    $dbname = "your_database_name";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // Get user input from the registration form
+    $email = $_POST["Email"];
+    $password = $_POST["Enter_Password"]; // Note: Ensure the name attribute in the HTML form matches here
+
+    // Insert user data into the database (update the table and column names)
+    $sql = "INSERT INTO users (email, password) VALUES ('$email', '$password')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Registration successful!";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    // Close the database connection
+    $conn->close();
+}
 ?>
